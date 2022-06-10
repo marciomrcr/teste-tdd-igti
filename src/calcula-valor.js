b53377a128eba683533dd8bcc625338d542bb23f
marciomrcr <marcio.mrcr@outlook.com>
2022-06-10 17:10:52 -0300

    configuração do Coverage 1.11 Cobertura de testes

--- src/calcula-valor.js
+++ src/calcula-valor.js
@@ -1,17 +1,37 @@
-function calcularMontante (capital, taxa, periodo) {
+function calcularMontante(capital, taxa, periodo) {
   let montante = capital * Math.pow(1 + taxa, periodo - 1)
   montante = arredondar(montante)
   return montante
 }
 
-function arredondar (valor) {
+function arredondar(valor) {
   const precisao = 100
   const arredondamento =
     Math.round((valor + Number.EPSILON) * precisao) / precisao
   return arredondamento
 }
 
+function calcularPrestacoes(montante, numeroParcelas) {
+  const prestacaoBase = arredondar(montante / numeroParcelas)
+  const resultado = Array(numeroParcelas).fill(prestacaoBase)
+
+  let somaPrestacoes = resultado.reduce((atual, total) => atual + total)
+  let diferenca = arredondar(montante - somaPrestacoes)
+  // Quando a diferenca >0 o fator é 1 senão é -1
+  const fator = diferenca > 0 ? 1 : -1
+
+  let i = diferenca > 0 ? 0 : resultado.length - 1
+  while (diferenca !== 0) {
+    resultado[i] = arredondar(resultado[i] + 0.01 * fator)
+    somaPrestacoes = resultado.reduce((atual, total) => atual + total)
+    diferenca = arredondar(montante - somaPrestacoes)
+    i += fator
+  }
+  return resultado
+}
+
 module.exports = {
   calcularMontante,
-  arredondar
+  arredondar,
+  calcularPrestacoes,
 }
--- test/calcula-valor.test.js
+++ test/calcula-valor.test.js
@@ -29,3 +29,80 @@ describe('arredondar', () => {
     expect(resultado).toBe(1.01)
   })
 })
+
+describe('calcularPrestacoes', () => {
+  test('O número de parcelas é igual ao numero de prestações', () => {
+    // Premissas
+    const numeroPrestacoes = 6
+    // Operações
+    const prestacoes = calculaValor.calcularPrestacoes(200, numeroPrestacoes)
+    // Resultado esperado
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+  })
+
+  test('Uma única prestação, valor é igual ao montante', () => {
+    // Premissas
+    const numeroPrestacoes = 1
+    // Operações
+    const prestacoes = calculaValor.calcularPrestacoes(200, numeroPrestacoes)
+    // Resultado esperado
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    expect(prestacoes[0]).toBe(200 / numeroPrestacoes)
+  })
+
+  test('2 prestações, valor é igual a 50% do montante', () => {
+    // Premissas
+    const numeroPrestacoes = 2
+    // Operações
+    const prestacoes = calculaValor.calcularPrestacoes(100, numeroPrestacoes)
+    // Resultado esperado
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    expect(prestacoes[0]).toBe(100 / numeroPrestacoes)
+    expect(prestacoes[0]).toBe(50)
+    expect(prestacoes[1]).toBe(50)
+  })
+
+  test('Valor da soma das prestações é igual ao montante com 2 casas decimais', () => {
+    // Dado (given)
+    const numeroPrestacoes = 3
+    const montante = 100
+    // Quando (when)
+    const prestacoes = calculaValor.calcularPrestacoes(
+      montante,
+      numeroPrestacoes
+    )
+    // Então (then)
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    const soma = calculaValor.arredondar(
+      prestacoes[0] + prestacoes[1] + prestacoes[2]
+    )
+    expect(soma).toBe(montante)
+
+    for (let i = 0; i < prestacoes.length - 1; i++) {
+      const j = i + 1
+      expect(prestacoes[i]).toBeGreaterThanOrEqual(prestacoes[j])
+    }
+  })
+
+  test('Desafio semi-final ', () => {
+    // Premissas Given
+    const numeroPrestacoes = 3
+    const montante = 101.994
+    // Quando When
+    const prestacoes = calculaValor.calcularPrestacoes(
+      montante,
+      numeroPrestacoes
+    )
+    // Então Then
+    expect(prestacoes.length).toBe(numeroPrestacoes)
+    const soma = calculaValor.arredondar(
+      prestacoes[0] + prestacoes[1] + prestacoes[2]
+    )
+    expect(soma).toBe(calculaValor.arredondar(montante))
+
+    for (let i = 0; i < prestacoes.length - 1; i++) {
+      const j = i + 1
+      expect(prestacoes[i]).toBeGreaterThanOrEqual(prestacoes[j])
+    }
+  })
+})

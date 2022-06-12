e4782edd1b82f8c0c519d656cbac45388f8c3ce5
marciomrcr <marcio.mrcr@outlook.com>
2022-06-12 17:42:45 -0300

    :test: retirado o coverege do Teste

--- consulta-credito/src/app.js
+++ consulta-credito/src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', async (req, res) => {
-  res.status(200).send('Boot camp no ar. Glória a Deus!')
+  res.status(200).send('APP no ar. Glória a Deus!')
 })
 
 module.exports = app

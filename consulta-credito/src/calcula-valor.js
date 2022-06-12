05d08dfe1b6abea87ab185eab40ea46a8f69a8c4
marciomrcr <marcio.mrcr@outlook.com>
2022-06-12 16:06:37 -0300

    Desisntalação eslint

--- consulta-credito/src/calcula-valor.js
+++ consulta-credito/src/calcula-valor.js
@@ -1,17 +1,17 @@
-function calcularMontante (capital, taxa, periodo) {
+function calcularMontante(capital, taxa, periodo) {
   let montante = capital * Math.pow(1 + taxa, periodo - 1)
   montante = arredondar(montante)
   return montante
 }
 
-function arredondar (valor) {
+function arredondar(valor) {
   const precisao = 100
   const arredondamento =
     Math.round((valor + Number.EPSILON) * precisao) / precisao
   return arredondamento
 }
 
-function calcularPrestacoes (montante, numeroParcelas) {
+function calcularPrestacoes(montante, numeroParcelas) {
   const prestacaoBase = arredondar(montante / numeroParcelas)
   const resultado = Array(numeroParcelas).fill(prestacaoBase)
 
@@ -19,8 +19,8 @@ function calcularPrestacoes (montante, numeroParcelas) {
   let diferenca = arredondar(montante - somaPrestacoes)
   // Quando a diferenca >0 o fator é 1 senão é -1
   const fator = diferenca > 0 ? 1 : -1
-
   let i = diferenca > 0 ? 0 : resultado.length - 1
+
   while (diferenca !== 0) {
     resultado[i] = arredondar(resultado[i] + 0.01 * fator)
     somaPrestacoes = resultado.reduce((atual, total) => atual + total)
@@ -33,5 +33,5 @@ function calcularPrestacoes (montante, numeroParcelas) {
 module.exports = {
   calcularMontante,
   arredondar,
-  calcularPrestacoes
+  calcularPrestacoes,
 }
